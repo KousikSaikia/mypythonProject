808e019f756d4a60a681907d918a47c8a13f5b1c
kousi <kousik.saikia@gmail.com>
2022-01-25 20:24:20 +0530

    first commmit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+var1 = 60
+
+
+
+def Cal_secs(n):
+    print(f"There are {n*var1} mins in a {n} hours")
+
+
+
+print("Enter No Of Days")
+var2 = input()
+Cal_secs(var2)
